2b1cbb9384ff108b71fe86e5b216f7ebffce8c3f
jason yu <condescendent@gmail.com>
2025-02-12 23:14:48 -0800

    removed one variable

--- aws-eks/tfc-workspace.tf
+++ aws-eks/tfc-workspace.tf
@@ -22,21 +22,18 @@ data "tfe_workspace" "aws-eks_workspace" {
 # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/variable
 resource "tfe_variable" "enable_aws_provider_auth" {
   workspace_id = data.tfe_workspace.aws-eks_workspace.id
-
   key      = "TFC_AWS_PROVIDER_AUTH"
   value    = "true"
   category = "env"
-
   description = "Enable the Workload Identity integration for AWS."
 }
 
-resource "tfe_variable" "tfc_aws_role_arn" {
-  workspace_id = data.tfe_workspace.aws-eks_workspace.id
-
-  key      = "TFC_AWS_RUN_ROLE_ARN"
-  #value    = aws_iam_role.tfc_role.arn
-  value    = data.aws_iam_role.tfc-role.arn
-  category = "env"
-
-  description = "The AWS role arn runs will use to authenticate."
-}
+#resource "tfe_variable" "tfc_aws_role_arn" {
+#  workspace_id = data.tfe_workspace.aws-eks_workspace.id
+#  key      = "TFC_AWS_RUN_ROLE_ARN"
+#  #value    = aws_iam_role.tfc_role.arn
+#  value    = data.aws_iam_role.tfc-role.arn
+#  category = "env"
+#
+#  description = "The AWS role arn runs will use to authenticate."
+#}
