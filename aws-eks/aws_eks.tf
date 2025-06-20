e74da26ea195d849168da78c65273c840443c8a2
Jason Yu <jia.yu@kyndryl.com>
2025-06-19 22:48:28 -0700

    updated the aws provider version number in two places

--- aws-eks/aws_eks.tf
+++ aws-eks/aws_eks.tf
@@ -59,3 +59,20 @@ module "eks" {
 #    }
   }
 }
+
+
+#module "karpenter" {
+#  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+#
+#  cluster_name = module.eks.cluster_name
+#
+#  # Attach additional IAM policies to the Karpenter node IAM role
+#  node_iam_role_additional_policies = {
+#    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+#  }
+#
+#  tags = {
+#    Environment = "dev"
+#    Terraform   = "true"
+#  }
+#}
