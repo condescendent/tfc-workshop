69f065a576845298919bb6c1324144dd41f5fa21
Jason Yu <jia.yu@kyndryl.com>
2025-08-04 23:06:46 -0700

    added helm_release for provisioning karpenter.

---
+++ aws-eks/helm_release.tf
@@ -0,0 +1,35 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "karpenter" {
+  name       = "karpenter"
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  namespace  = "karpenter"
+  version    = "v0.36.1" # Match this to latest stable if needed
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      serviceAccount = {
+        name = "karpenter"
+        annotations = {
+          "eks.amazonaws.com/role-arn" = module.karpenter.iam_role_arn
+        }
+      }
+
+      settings = {
+        clusterName    = module.eks.cluster_name
+        clusterEndpoint = module.eks.cluster_endpoint
+        aws = {
+          defaultInstanceProfile = module.karpenter.karpenter_node_instance_profile_name
+        }
+      }
+    })
+  ]
+}
